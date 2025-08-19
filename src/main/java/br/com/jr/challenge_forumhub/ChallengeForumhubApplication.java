aae07ff5ccf9d432b63a9ad47d97ac49c547e2d7
José Romildo <pronuno1@gmail.com>
2025-08-19 11:46:35 -0300

    Configuração inicial do Projeto

---
+++ src/main/java/br/com/jr/challenge_forumhub/ChallengeForumhubApplication.java
@@ -0,0 +1,13 @@
+package br.com.jr.challenge_forumhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForumhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForumhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jr/challenge_forumhub/ChallengeForumhubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jr.challenge_forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForumhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
