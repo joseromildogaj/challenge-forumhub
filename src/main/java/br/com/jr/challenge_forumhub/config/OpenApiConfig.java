115e7ac478e50a8496b93829ed8e9d98c5f8f077
José Romildo <pronuno1@gmail.com>
2025-08-19 14:15:53 -0300

    Configuração do Swagger para documentação da API

---
+++ src/main/java/br/com/jr/challenge_forumhub/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package br.com.jr.challenge_forumhub.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Forum Hub API",
+                version = "1.0",
+                description = "Documentação da API do Forum Hub, um projeto de fórum online."
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+
+}
